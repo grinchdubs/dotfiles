a96c2ec9f4ba9491819bec9ab740341f19c6ad2d
grinchdubs <cubanb95@gmail.com>
2024-06-22 23:50:13 -0400

    Initial commit

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2024-06-17 00:33:03
+set PATH $PATH /home/grnch/.local/bin
+zoxide init --cmd cd fish | source
+alias ls=eza
+alias cat=batcat
