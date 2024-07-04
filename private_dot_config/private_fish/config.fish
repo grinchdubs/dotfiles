4b68b1b6dbc1a23bcdfa3df5cf3d2d0c4c00b8de
GRNCH <cubanb95@gmail.com>
2024-07-04 15:23:08 -0400

    Changes to fish, sway and waybar configs

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,9 +4,14 @@ end
 
 # Created by `pipx` on 2024-06-17 00:33:03
 set PATH $PATH /home/grnch/.local/bin
+
+#Inits
 zoxide init --cmd cd fish | source
+atuin init --disable-ctrl-r fish | source
+
+#Aliases
 alias ls=eza
 alias cat=batcat
 alias bat=batcat
 alias cdz='zoxide query --interactive'
-atuin init --disable-ctrl-r fish | source
+alias fuck=thefuck
